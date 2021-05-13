5009d6f1f290445a1714ba761050fc72651b4384
Alessandro Carvalho <alessandro.acarvalho@al.infnet.edu.br>
2021-05-13 08:43:25 -0300

    Add project files.

---
+++ Aula2ConsoleVs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Aula2ConsoleVs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Started!");
+
+            Console.Write("Enter a number: ");
+
+            var numberText : string = Console.ReadLine();
+
+            var parseDidntWork : bool = !Int32.TryParse(numberText, out var number :int);
+
+            if (parseDidntWork)
+            {
+                Console.WriteLine("NaN!");
+                Console.ReadKey();
+                return;
+
+            }
+            if (number == 1)
+            {
+                Console.WriteLine("Number is one!");
+
+            }
+            else
+            {
+                Console.WriteLine("Not one!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
